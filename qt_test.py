0aaabb6c323781eb72780c8ab0845ff7089c70d3
fujie <1243596620@qq.com>
2021-06-18 20:01:57 +0800

    图片接受

--- qt_test.py
+++ qt_test.py
@@ -1,10 +1,55 @@
-import json
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
 
-json_str = '{"id":"myid007","message":"test","type":"TEXT"}'
-test = [-119, 80, 78, 71, 13, 10, 26, 10, 0, 0, 0, 13, 73, 72]
+"""
+ZetCode PyQt5 tutorial 
 
-print(len(test))
-b = bytes(test)
-print(b)
-# data_bytes = bytes(test)
-# print(data_bytes)
+In this example, we create a more 
+complicated window layout using
+the QGridLayout manager. 
+
+Author: Jan Bodnar
+Website: zetcode.com 
+Last edited: August 2017
+"""
+
+import sys
+from PyQt5.QtWidgets import (QWidget, QLabel, QLineEdit,
+                             QTextEdit, QGridLayout, QApplication, QHBoxLayout, QVBoxLayout)
+from PyQt5.QtGui import QPixmap
+
+
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        title = QLabel('Title')
+        author = QLabel('Author')
+        review = QLabel('Review')
+
+        titleEdit = QLineEdit()
+        authorEdit = QLineEdit()
+        reviewEdit = QTextEdit()
+
+        grid = QGridLayout()
+        grid.setSpacing(10)
+
+        hbox = QHBoxLayout(self)
+        pixmap = QPixmap("redrock.png")
+
+        grid.addWidget(title, 1, 0)
+
+        self.setLayout(grid)
+
+        self.setGeometry(300, 300, 350, 600)
+        self.setWindowTitle('Review')
+        self.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
